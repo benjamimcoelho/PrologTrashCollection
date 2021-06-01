9847521b1f486d657500ef9cb786295071e964f2
Benjamim Coelho <nyblen2000@gmail.com>
2021-06-01 21:39:59 +0100

    parser finished

--- dataset_lex.py
+++ dataset_lex.py
@@ -1,29 +1,46 @@
 import ply.lex as lex
 
+reserved = {
+    'Par ': 'PAR',
+    'Impar ': 'IMPAR',
+    'Ambos ': 'AMBOS',
+}
 
-tokens = ['FLOAT', 'INT', 'WORD']
+tokens = ['FLOAT', 'INT', 'WORD', 'LIXO', 'SEQ'] + list(reserved.values())
 
-literals = [',', ':', '(', ')', '-']
+literals = [',', ':', ')', '(', '-']
+
+
+def t_WORD(t):
+    r'\s*[a-zA-Z][\.\d\w\s]*'
+    t.type = reserved.get(t.value, 'WORD')    # Check for reserved words
+    return t
 
 
 t_FLOAT = r'[\-+]?\d+\.\d+'
-t_INT = r'\d+'
-t_WORD = r'[a-zA-Z\s]+'
+t_INT = r'\s*\d+\s*'
+t_LIXO = r'\(\d*\-\>\d*\)\(\d*\-\>\d*\)'
+t_SEQ = r'\s*(\d+\-)+\d+'
 
 
 def t_error(t):
+    #global l
     print("Carater ilegal: ", t.value[0])
+    #print('Linha :' + str(l))
     t.lexer.skip(1)
 
 
-t_ignore = " \t\n\">/º"
+t_ignore = "\t\n\"/º"
 lexer = lex.lex()
 
 # PARA TESTAR O ANALISADOR LEXICO:
-
+"""
+l = 0
 import sys
 with open('dataset.csv') as input:
     for linha in input:
         lexer.input(linha)
         for tok in lexer:
             print(tok)
+        l += 1
+"""
---
+++ dataset_yacc.py
@@ -0,0 +1,191 @@
+import ply.yacc as yacc
+from dataset_lex import tokens
+from dataset_lex import literals
+import sys
+import re
+
+last_street = ''
+info = []
+pontoId = 0
+
+
+def deleteNewLine(string):
+    return re.sub(r'\n', '', string)
+
+
+def strip_one_space(s):
+    if s.endswith(" "):
+        s = s[:-1]
+    if s.startswith(" "):
+        s = s[1:]
+    return s
+
+
+def exportArestas(arestas):
+    with open("arestas.txt", 'w') as f:
+        for i in range(len(arestas)):
+            f.write('aresta( \'' + strip_one_space(arestas[i][0]) +
+                    '\' ,' + ' \'' + strip_one_space(arestas[i][1]) + '\' ).\n')
+
+
+def exportPontosRecolha(pontos_recolha):
+    with open("pontosRecolha.txt", 'w') as f:
+        for i in pontos_recolha.items():
+            f.write('pontoRecolha( ' + i[0] + ' , ' + i[1][0] + ' , ' + i[1][1] +
+                    ' , \'' + i[1][2] + '\' , \'' + strip_one_space(i[1][3]) + '\' , ' + str(i[1][4]) + ').\n')
+
+# id -> [latitude,longitude,Freguesia,rua,[(tipo residuo,tipo_contentor,litros)...]]
+
+
+def contains(list, elem):
+    for i in range(len(list)):
+        if list[i] == elem:
+            return True
+    return False
+
+
+def addIfNew(list, elem):
+    if not contains(list, elem) and elem[0] != elem[1]:
+        list.append(elem)
+
+
+def updateContentor():
+    global last_street, info, pontoId
+    if pontoId in parser.pontos_recolha:  # se o ponto de recolha já existir, so temos de adicionar o contentor
+        aux = parser.pontos_recolha.get(pontoId)
+        aux2 = aux[4]
+        if not contains(aux2, info[4][0]):
+            aux2.append(info[4][0])
+            aux[4] = aux2
+            parser.pontos_recolha.update({pontoId: aux})
+    else:  # se o ponto de recolha ainda n existir temos de adicioná-lo
+        parser.pontos_recolha.update({pontoId: info})
+
+
+def p_Entry(p):
+    "Entry : FLOAT ',' FLOAT ',' INT ',' WORD ',' RUAS ',' WORD ',' WORD ',' INT ',' INT ',' INT"
+    global info
+    p[0] = p[1] + p[3] + p[5] + p[7] + p[9] + \
+        p[11] + p[13] + p[15] + p[17] + p[19]
+    info[0] = p[1]
+    info[1] = p[3]
+    info[2] = p[7]
+    info[4] = [(p[11], p[13], deleteNewLine(p[19]))]
+
+    updateContentor()
+
+
+def p_RUAS_Par(p):
+    "RUAS : INT ':' WORD '(' PAR LIXO ':' WORD '-' WORD ')'"
+    global last_street, info, pontoId
+    p[0] = 'ruas par'
+    addIfNew(parser.arestas, (p[8], p[3]))
+    addIfNew(parser.arestas, (p[3], p[10]))
+    last_street = p[3]
+    info = [0, 0, 0, p[3], [], 0]
+    pontoId = p[1]
+
+
+def p_RUAS_Impar(p):
+    "RUAS : INT ':' WORD '(' IMPAR LIXO ':' WORD '-' WORD ')'"
+    global last_street, info, pontoId
+    p[0] = 'ruas impar'
+    addIfNew(parser.arestas, (p[10], p[3]))
+    addIfNew(parser.arestas, (p[3], p[8]))
+    last_street = p[3]
+    info = [0, 0, 0, p[3], []]
+    pontoId = p[1]
+
+
+def p_RUAS_Ambos(p):
+    "RUAS : INT ':' WORD '(' AMBOS LIXO ':' WORD '-' WORD ')'"
+    global last_street, info, pontoId
+    p[0] = 'ruas ambos'
+    addIfNew(parser.arestas, (p[8], p[3]))
+    addIfNew(parser.arestas, (p[3], p[10]))
+    addIfNew(parser.arestas, (p[10], p[3]))
+    addIfNew(parser.arestas, (p[3], p[8]))
+    last_street = p[3]
+    info = [0, 0, 0, p[3], []]
+    pontoId = p[1]
+
+
+def p_RUAS_rua_rua(p):
+    "RUAS : INT ':' WORD ',' WORD"
+    global last_street, info, pontoId
+    p[0] = 'rua'
+    addIfNew(parser.arestas, (p[3], p[5]))
+    addIfNew(parser.arestas, (p[3], last_street))
+    addIfNew(parser.arestas, (last_street, p[3]))
+    last_street = p[3]
+    info = [0, 0, 0, p[3], []]
+    pontoId = p[1]
+
+
+def p_RUAS_rua_int(p):
+    "RUAS : INT ':' WORD ',' INT"
+    global last_street, info, pontoId
+    p[0] = 'rua'
+    addIfNew(parser.arestas, (p[3], last_street))
+    addIfNew(parser.arestas, (last_street, p[3]))
+    last_street = p[3]
+    info = [0, 0, 0, p[3], []]
+    pontoId = p[1]
+
+
+def p_RUAS_rua_seq(p):
+    "RUAS : INT ':' WORD ',' SEQ"
+    global last_street, info, pontoId
+    p[0] = 'rua'
+    addIfNew(parser.arestas, (p[3], last_street))
+    addIfNew(parser.arestas, (last_street, p[3]))
+    last_street = p[3]
+    info = [0, 0, 0, p[3], []]
+    pontoId = p[1]
+
+
+def p_RUAS_rua_INT_WORD(p):
+    "RUAS : INT ':' WORD ',' INT WORD"
+    global last_street, info, pontoId
+    p[0] = 'rua'
+    addIfNew(parser.arestas, (p[3], last_street))
+    addIfNew(parser.arestas, (last_street, p[3]))
+    last_street = p[3]
+    info = [0, 0, 0, p[3], []]
+    pontoId = p[1]
+
+
+def p_error(p):
+    global l
+    print("Syntax error in input: ", p)
+    print('linha ' + str(l))
+
+
+parser = yacc.yacc()
+
+
+# id -> [latitude,longitude,Freguesia,rua,[(tipo residuo,tipo_contentor,litros)...]]
+parser.pontos_recolha = {}
+
+
+# rua -> rua (significa que o camiao viaja no sentido esquerda para a direita)
+parser.arestas = []
+total = 0
+l = 0
+
+
+with open('dataset.csv') as input:
+    for linha in input:
+        result = parser.parse(linha)
+        if result:
+            # print('entry valida: \n\n' + result)
+            total += 1
+        l += 1
+
+print('TOTAL ENTRIES VÁLIDAS: ' + str(total))
+# print(parser.arestas)
+# print(str(len(parser.arestas)))
+# print(parser.pontos_recolha)
+# print(str(len(parser.pontos_recolha)))
+exportArestas(parser.arestas)
+exportPontosRecolha(parser.pontos_recolha)
